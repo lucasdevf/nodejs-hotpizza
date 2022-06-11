7687d1cae9835297b1bceaed2e3d52b160736a95
Lucas <lucaasferreirafl@gmail.com>
2022-06-10 23:16:45 -0300

    feat: add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
+import 'express-async-errors'
+import { Request, Response, NextFunction, response } from 'express'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    error: 'Internal Server Error'
+  })
+
+})
 
 export { app }
